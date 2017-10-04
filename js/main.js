d8308ceff9d3816db213fa5efa8855ce780dd8fc
Miguel Viegas <miegas4@users.noreply.github.com>
2017-10-04 09:56:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,9 +52,11 @@ function createScene() {
 	var table = new Table(1000);
 	var light = new DirectionalLight();
 	var track = new Track();
+	var mouse_Car = new CarMouse(10);
 
 	scene.add(new THREE.AxisHelper(10));
-	scene.add(createCarRound(0,20,0, 10));
+	scene.add(mouse_Car.getMesh(0, 20, 0));
+	scene.add(mouse_Car.getMesh(0, 50, 0));
 	scene.add(table.create_table());
 	scene.add(createCar(30, 0, 30, 3));
 	scene.add(track.create_track());
