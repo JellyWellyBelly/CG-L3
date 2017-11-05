193c219fc76644fae44b0bee43f0f48da95d9ccb
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2017-11-05 18:45:43 +0000

    Update butter.js

--- js/butter.js
+++ js/butter.js
@@ -22,7 +22,7 @@ class Butter extends MovableObject {
 
   addBase(obj, size, x, y, z) {
     var geometry = new THREE.BoxGeometry(size, size / 4, 2 * size);
-    var material = new THREE.MeshLambertMaterial({color: 0x6d4e04, wireframe: false});
+    var material = new THREE.MeshPhongMaterial({color: 0x6d4e04, wireframe: false});
     var mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.set(0, size / 8, 0);
@@ -33,7 +33,7 @@ class Butter extends MovableObject {
 
   addButter(obj, size, x, y, z) {
     var geometry = new THREE.BoxGeometry(0.8 * size, 0.4 * size, 1.25 * size);
-    var material = new THREE.MeshLambertMaterial( {color: 0xefea83, wireframe: false});
+    var material = new THREE.MeshPhongMaterial( {color: 0xefea83, wireframe: false});
     var mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.set(0, size / 4 + 0.2 * size , 0.3 * size);
