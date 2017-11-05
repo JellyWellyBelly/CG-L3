919fdd96a6523a457e5b1196ed688f28a7ff5622
Miguel Viegas <miegas4@users.noreply.github.com>
2017-11-05 21:11:35 +0000

    Add files via upload

--- js/table.js
+++ js/table.js
@@ -1,55 +1,56 @@
 'use strict';
 
-class Table {
+class Table extends MovableObject{
 
-  constructor(size) {
-    this._size = size;
-  }
+    constructor(size) {
+        var height = size / 10;
+        var table = new THREE.Object3D();
+        
+        super(0, 0, 0, 0, 0, 0, size, table, 0);
 
-  create_table() {
-    var size = this._size;
-    var height = size / 10;
+        this.addTableTop(table, size);
+        this.addLeg(table, size / 4, size / 4);
+        this.addLeg(table, size / 4, -size / 4);
+        this.addLeg(table, -size / 4, size / 4);
+        this.addLeg(table, -size / 4, -size / 4);
+        this.addFloor(table, size);
+    }
 
-  	var geometry = new THREE.BoxGeometry(size, height, size, size/50, height/50, size/50);
-  	var material = new THREE.MeshPhongMaterial({color: 0x8B4513, shininess: 10});
-  	var table = new THREE.Mesh(geometry, material);
-  	
-  	table.position.set(0, -height / 2, 0);
+    addTableTop(table, size) {
+        var height = this._size / 10;
 
-    this.addLeg(table, size / 4, size / 4);
-    this.addLeg(table, size / 4, -size / 4);
-    this.addLeg(table, -size / 4, size / 4);
-    this.addLeg(table, -size / 4, -size / 4);
-    this.addFloor(table);
+        var geometry = new THREE.BoxGeometry(size, height, size, size/50, height/50, size/50);
+        var material = new THREE.MeshPhongMaterial({color: 0x8B4513, shininess: 10});
+        var tableTop = new THREE.Mesh(geometry, material);
+        
+        tableTop.position.set(0, -height / 2, 0);
+        
+        table.add(tableTop);
+    }
 
-    table.receiveShadow = true;
+    addLeg(table, x, z) {
+        var height = this._size / 10;
+        
+        var geometry = new THREE.BoxGeometry(height, 2 * height, height, height/50, height/25, height/50);
+        var material = new THREE.MeshPhongMaterial({color: 0x8B4513});
+        var leg = new THREE.Mesh(geometry, material);
 
-  	return table;
-  }
+        leg.position.set(x, -(3 * height) / 2, z);
 
-  addLeg(table, x, z) {
-    var height = this._size / 10;
-    
-    var geometry = new THREE.BoxGeometry(height, 2 * height, height, height/50, height/25, height/50);
-    var material = new THREE.MeshPhongMaterial({color: 0x8B4513});
-    var leg = new THREE.Mesh(geometry, material);
+        table.add(leg);
+    }
 
-    leg.position.set(x, -(3 * height) / 2, z);
+    addFloor(table, size) {
+        var height = this._size / 10;
+        
+        var geometry = new THREE.PlaneGeometry(size*10, size*10);
+        var material = new THREE.MeshPhongMaterial({color: 0xaf7a1d});
+        var floor = new THREE.Mesh(geometry, material);
 
-    table.add(leg);
-  }
+        
+        floor.rotation.x = -Math.PI / 2;
+        floor.position.set(0, -(3 * height), 0);
 
-  addFloor(table) {
-    var height = this._size / 10;
-    
-    var geometry = new THREE.PlaneGeometry(3000, 3000);
-    var material = new THREE.MeshPhongMaterial({color: 0xaf7a1d});
-    var floor = new THREE.Mesh(geometry, material);
-
-    
-    floor.rotation.x = -Math.PI / 2;
-    floor.position.set(0, -(3 * height), 0);
-
-    table.add(floor);
-  }
+        table.add(floor);
+    }
 }
