206c8c8392074c56c2afbef89c56eaca8077f7c1
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2017-11-05 16:33:52 +0000

    Update table.js

--- js/table.js
+++ js/table.js
@@ -11,7 +11,7 @@ class Table {
     var height = size / 10;
 
   	var geometry = new THREE.BoxGeometry(size, height, size, size/50, height/50, size/50);
-  	var material = new THREE.MeshStandardMaterial({color: 0x000991});
+  	var material = new THREE.MeshPhongMaterial({color: 0x000991});
   	var table = new THREE.Mesh(geometry, material);
   	
   	table.position.set(0, -height / 2, 0);
@@ -21,7 +21,7 @@ class Table {
     this.addLeg(table, -size / 4, size / 4);
     this.addLeg(table, -size / 4, -size / 4);
 
-    table.castShadow = true;
+    table.receiveShadow = true;
 
   	return table;
   }
@@ -30,7 +30,7 @@ class Table {
     var height = this._size / 10;
     
     var geometry = new THREE.BoxGeometry(height, 2 * height, height, height/50, height/25, height/50);
-    var material = new THREE.MeshLambertMaterial({color: 0x000991});
+    var material = new THREE.MeshPhongMaterial({color: 0x000991});
     var leg = new THREE.Mesh(geometry, material);
 
     leg.position.set(x, -(3 * height) / 2, z);
